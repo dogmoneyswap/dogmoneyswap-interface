0965122238709988bab527bc9dc136559630008e
Kasumi <kasumi.null@yandex.com>
2022-08-17 15:37:57 -0500

    feat: add social links

--- src/components/Header/More.tsx
+++ src/components/Header/More.tsx
@@ -25,13 +25,13 @@ const items = (i18n: I18n) => [
   {
     name: i18n._(t`Discord`),
     description: i18n._(t`Join the community on Discord.`),
-    href: 'https://discord.gg/xFTnQEzu9T',
+    href: 'https://discord.gg/EKpGzWHRu4',
     external: true,
   },
   {
     name: i18n._(t`Telegram`),
     description: i18n._(t`Join the community on Telegram.`),
-    href: 'https://t.me/MistSwapOfficial',
+    href: 'https://t.me/DogMoneyOfficial',
     external: true,
   },
 ]
