5af4a37678d576d695cd110d08706a8d50eb0ec7
Kasumi <kasumi.null@yandex.com>
2022-08-18 21:01:26 -0500

    feat: update branding

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,47 +33,13 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en" dir="ltr">
         <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+          <link rel="manifest" href="/site.webmanifest">
+          <meta name="msapplication-TileColor" content="#da532c">
+          <meta name="theme-color" content="#ffffff">
 
-          <link rel="manifest" href="/site.webmanifest" />
-
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="180x180" href="/apple-touch-icon-precomposed.png" />
-
-          <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/apple-touch-icon-57x57-precomposed.png" />
-
-          <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/apple-touch-icon-60x60-precomposed.png" />
-
-          <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/apple-touch-icon-72x72-precomposed.png" />
-
-          <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/apple-touch-icon-76x76-precomposed.png" />
-
-          <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/apple-touch-icon-114x114-precomposed.png" />
-
-          <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/apple-touch-icon-120x120-precomposed.png" />
-
-          <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/apple-touch-icon-144x144-precomposed.png" />
-
-          <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/apple-touch-icon-152x152-precomposed.png" />
-
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="180x180" href="/apple-touch-icon-180x180-precomposed.png" />
-
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="shortcut icon" href="/favicon.ico" />
-
-          <link rel="manifest" href="/manifest.json" />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
