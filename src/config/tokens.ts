51ed30fc97517d67d7846688b2d2785e4a2755ee
mainnet-pat <mainnet_pat@protonmail.com>
2022-08-19 13:55:28 +0000

    feat: fix tvl calculation

--- src/config/tokens.ts
+++ src/config/tokens.ts
@@ -3,6 +3,7 @@ import { ChainId, MIST_ADDRESS, BAR_ADDRESS, Token, WBCH} from '@dogmoneyswap/sd
 export const DAI = new Token(ChainId.DOGECHAIN, '0x639A647fbe20b6c8ac19E48E2de44ea792c62c5C', 18, 'DAI', 'Dai Stablecoin')
 export const USDT = new Token(ChainId.DOGECHAIN, '0xE3F5a90F9cb311505cd691a46596599aA1A0AD7D', 6, 'USDT', 'Tether USD')
 export const USDC = new Token(ChainId.DOGECHAIN, '0x765277EebeCA2e31912C9946eAe1021199B39C61', 6, 'USDC', 'USD Coin')
+export const WBTC = new Token(ChainId.DOGECHAIN, '0xfA9343C3897324496A05fC75abeD6bAC29f8A40f', 18, 'WBTC', 'Wrapped Bitcoin')
 
 export const XMIST: ChainTokenMap = {
     [ChainId.DOGECHAIN]: new Token(ChainId.DOGECHAIN, BAR_ADDRESS[ChainId.DOGECHAIN], 18, 'xDOGMONEY', 'Staked Dog Money'),
